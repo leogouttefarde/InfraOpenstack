a4c59e598e7ade70e3f33e6f418b997e685ace58
Mat's <mathieu.stoffel@ensimag.grenoble-inp.fr>
2017-01-04 14:47:57 +0100

    [Management] Creation of br-ex (NOT TESTED)
    In order to be tested, the provisioning of OpenVSwitch must be done.

---
+++ management/set_brex.sh
@@ -0,0 +1,110 @@
+#! /bin/bash
+
+# This script fails if any command does.
+set -eo pipefail
+
+# Defining global parameters.
+MANAGEMENT_DIR=$(cd $(dirname $0) && pwd)
+NETWORK_CONFIG_DIR="/etc/sysconfig/network-scripts"
+ENO_1_CONFIG="${NETWORK_CONFIG_DIR}/ifcfg-eno1"
+BR_EX_CONFIG="${NETWORK_CONFIG_DIR}/ifcfg-br-ex"
+BASE_IP=10.11.51
+BASE_HOST=160
+NETMASK=255.255.255.192
+GATEWAY=10.11.51.129
+DNS1=10.3.252.26
+
+# Handling the arguments.
+TARGET=0
+while getopts ":hn:" arg; do
+    case "$arg" in
+        h)
+            echo "
+./set_brex.sh
+
+List of options:
+-h      => prints the associated help;
+-n id   => specifies the ID of the targetted server.
+
+Creates the OpenVSwitch interface required by OpenStack on the
+targetted server.
+
+As a consequence, it is within the range 1 <= ID <= 14 (\"1\" 
+corresponding to \"c31\" and \"14\" to \"c44\").
+"
+            exit 0
+            ;;
+        n)
+            if [[ "$OPTARG" -le 0 || "$OPTARG" -ge 15 ]]; then
+                echo "The ID must be within [[1 ; 14]]."
+                echo "Try ./connect.sh -h."
+                exit 1
+            fi
+            TARGET="$OPTARG"
+            ;;
+        :)
+            echo "The option \"${arg}\" needs an argument."
+            echo "Try ./connect.sh -h."
+            exit 1
+            ;;
+        \?)
+            echo "Unknown option: \"${OPTARG}\"."
+            echo "Try ./connect.sh -h."
+            exit 1
+            ;;
+    esac
+done
+
+# Verifying that the ID of the targetted server has been set.
+if [[ "$TARGET" -eq 0 ]]; then
+    echo "The ID of the targetted server must be set."
+    echo "Try \"./set_brex.sh -h\"."
+    exit 1
+fi
+# Computing the IP address of the remote server (useful for the
+# configuration files of the network interfaces.
+let "ip_addr = BASE_HOST + TARGET"
+ip_addr="${BASE_IP}.${ip_addr}"
+
+# Sending the associated commands over SSH.
+echo "
+Creating and configuring the OpenVSwitch required by OpenStack.
+
+Since it requires to restart the network interfaces, you shall
+be disconnected from the remote host at the end of the execution
+of this script.
+
+Thus, if a message related to such a disconnection occurs, please
+ignore it.
+"
+
+cd "${MANAGEMENT_DIR}/../connection"
+
+echo -e "\nCreating the OpenVSwitch ...\n"
+./connect.sh -n "$TARGET" -c "ovs-vsctl add-br br-ex && \
+    ovs-vsctl add-port eno1"
+
+
+echo -e "\nModifying the configuration file related to the physical 
+interface ...\n"
+
+./connect.sh -n "$TARGET" -c "echo \"DEVICE=eno1
+TYPE=OVSPORT
+DEVICETYPE=ovs
+OVS_BRIDGE=br-ex
+ONBOOT=yes
+BOOTPROTO=static\" > ${ENO_1_CONFIG}"
+
+echo -e "\nCreating the configuration file related to the OpenVSwitch ...\n"
+./connect.sh -n "$TARGET" -c "echo \"DEVICE=br-ex
+DEVICETYPE=ovs
+TYPE=OVSBridge
+BOOTPROTO=static
+IPADDR=${ip_addr}
+NETMASK=${NETMASK}
+GATEWAY=${GATEWAY}
+DNS1=${DNS1}
+ONBOOT=yes\" > ${BR_EX_CONFIG}"
+
+echo -e "\nRestarting the network interface ...\n"
+systemctl restart network
