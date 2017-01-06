a576e7317c0ce46c9c9f6eddd4e4bf796e4534fd
Guillaume Mallet <guillaume.mallet@ensimag.grenoble-inp.fr>
2017-01-06 14:25:50 +0100

    Add network provision script for removing network manager on nodes

---
+++ provision/network/network.sh
@@ -0,0 +1,20 @@
+#! /bin/bash
+
+# Defining the global parameters of the script.
+LANGUAGE_ENVIRONMENT_FILE="/etc/environment"
+
+# Appending the language environment settings.
+echo "
+# START OF PAIO CONFIG
+LANG=en_US.utf-8
+LC_ALL=en_US.utf-8
+# END OF PAIO CONFIG" >> "$LANGUAGE_ENVIRONMENT_FILE"
+
+# Setting up network so as to allow connection from the outside.
+systemctl disable firewalld
+systemctl stop firewalld
+systemctl disable NetworkManager
+systemctl stop NetworkManager
+systemctl enable network
+systemctl start network
+
