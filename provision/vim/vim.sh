5a52620c54b368d693ae089df9d11710ad2fbca9
Mat's <mathieu.stoffel@ensimag.grenoble-inp.fr>
2017-01-12 23:18:00 +0100

    [Provision] Provisioning Vim in a Screen session

--- provision/vim/vim.sh
+++ provision/vim/vim.sh
@@ -1,5 +1,40 @@
 #! /bin/bash
 
+# Handling the arguments of the script.
+USE_SCREEN=0
+while getopts ":hS" arg; do
+    case "$arg" in
+        h)
+            echo "
+./vim.sh
+
+List of options:
+-h => prints the associated help;
+-S => activate the in-a-screen-session provisioning option.
+
+Provisions the designated host with Vim.
+"
+            exit 0
+            ;;
+        S)
+            USE_SCREEN=1
+            ;;
+        \?)
+            echo "Unknown option: \"${OPTARG}\"."
+            echo "Try ./vim.sh -h."
+            exit 1
+            ;;
+    esac
+done
+
+# Executing this script in a screen session, in order to prevent
+# a network crash from crashing the update/install process.
+if [[ $USE_SCREEN -eq 1 ]]; then
+    if [ -z "$STY" ]; then 
+        exec screen -dm -S provision /bin/bash "$0"; 
+    fi
+fi
+
 # Defining global parameters of this script.
 VIMRC_URL="https://gist.githubusercontent.com/matsKNY/\
 696bb092e52ff61925e1136baaa2cdc6/raw/\
