fceb580bff4a7529971543825276e52eada3f4c4
Mat's <mathieu.stoffel@ensimag.grenoble-inp.fr>
2017-01-12 23:18:45 +0100

    [Provision] Provisioning PAIO in a Screen session

--- provision/packstack-all_in_one/packstack-all_in_one.sh
+++ provision/packstack-all_in_one/packstack-all_in_one.sh
@@ -1,5 +1,40 @@
 #! /bin/bash
 
+# Handling the arguments of the script.
+USE_SCREEN=0
+while getopts ":hS" arg; do
+    case "$arg" in
+        h)
+            echo "
+./packstack-all_in_one.sh
+
+List of options:
+-h => prints the associated help;
+-S => activate the in-a-screen-session provisioning option.
+
+Provisions the designated host with OpenVSwitch.
+"
+            exit 0
+            ;;
+        S)
+            USE_SCREEN=1
+            ;;
+        \?)
+            echo "Unknown option: \"${OPTARG}\"."
+            echo "Try ./packstack-all_in_one.sh -h."
+            exit 1
+            ;;
+    esac
+done
+
+# Executing this script in a screen session, in order to prevent
+# a network crash from crashing the update/install process.
+if [[ $USE_SCREEN -eq 1 ]]; then
+    if [ -z "$STY" ]; then 
+        exec screen -dm -S provision /bin/bash "$0"; 
+    fi
+fi
+
 # Defining the global parameters of the script.
 LANGUAGE_ENVIRONMENT_FILE="/etc/environment"
 
